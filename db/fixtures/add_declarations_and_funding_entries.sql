7575679821cbc682b169575d8f86138a8f9028bd
johnnylarner <james.richardson.2556@gmail.com>
2026-09-24 16:54:13 +0200

    tests: fix bad test and clean up slop

--- db/fixtures/add_declarations_and_funding_entries.sql
+++ db/fixtures/add_declarations_and_funding_entries.sql
@@ -25,13 +25,14 @@ INSERT INTO exposed.declarations (
 
 INSERT INTO exposed.funders (funder_name, funder_kind, company_number)
 VALUES
-    ('McDonald''s', 'Company', '123'),
+    (E'McDonald\'s', 'Company', '123'),
     ('Aaron Banks', 'Individual', NULL);
+
 
 INSERT INTO exposed.funding_entries (
     source_declaration_id, funder_id, amount, currency, payment_type
 ) VALUES
-    (1, (SELECT id FROM exposed.funders WHERE funder_name = 'McDonald''s'),
+    (1, (SELECT id FROM exposed.funders WHERE funder_name = E'McDonald\'s'),
      10, 'GBP', 'Monetary'),
     (2, (SELECT id FROM exposed.funders WHERE funder_name = 'Aaron Banks'),
      100, 'GBP', 'In Kind');
